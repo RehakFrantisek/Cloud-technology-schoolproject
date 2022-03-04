23714e7640e9adb50bd4bd341653e15d18d09a4c
frantisek.rehak <frantisek.rehak@tul.cz>
2022-03-04 21:26:30 +0100

    fibonacci closure

--- cv01_fibonacci.go
+++ cv01_fibonacci.go
@@ -5,6 +5,12 @@ import "fmt"
 // fibonacci is a function that returns
 // a function that returns an int.
 func fibonacci() func() int {
+	x, y := 1, 0
+	return func() int {
+		tmp := y
+		x, y = y, x+y
+		return tmp
+	}
 }
 
 func main() {
