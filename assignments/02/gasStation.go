bbe0c1c40ba3194d4c209f0fc2f561c3ebd76bf1
frantisek.rehak <frantisek.rehak@tul.cz>
2022-04-08 17:36:53 +0200

    gasStation functional solution

--- assignments/02/gasStation.go
+++ assignments/02/gasStation.go
@@ -2,28 +2,50 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
+	"time"
 )
 
-func pump(spots int, fuel_type string, group *sync.WaitGroup) {
+func pump(fuelType string, wg *sync.WaitGroup, minMilSecsWait, maxMilSecsWait int, pumpCap chan int) {
+	defer wg.Done()
+	cashDesk := make(chan int, 2)
+	carQueue := 3
 	var wg2 sync.WaitGroup
-	for i := 0; i < spots; i++ {
+	for i := 0; i < carQueue; i++ {
+		pumpCap <- 1
 		wg2.Add(1)
-		go func(n int) {
-			fmt.Println("test")
-		}(i)
-		wg2.Done()
+		waitTime := rand.Intn(maxMilSecsWait-minMilSecsWait) + minMilSecsWait
+		go func() {
+			cashDesk <- 1
+			carServe(fuelType, waitTime, &wg2)
+			<-pumpCap
+			<-cashDesk
+		}()
 	}
+	wg2.Wait()
+}
+
+func carServe(fuelT string, waitTime int, wg2 *sync.WaitGroup) {
+	defer wg2.Done()
+	fmt.Println("Car with fuel", fuelT, "fueling..")
+	time.Sleep(time.Duration(waitTime) * time.Millisecond)
+	fmt.Println("Car with fuel", fuelT, "paying..")
+	time.Sleep(time.Duration(rand.Intn(2-1)+1) * time.Millisecond)
 }
 
 func main() {
+	pumpCap := make(chan int, 4)
+	elCap := make(chan int, 8)
+	lpgCap := make(chan int, 1)
 	var wg sync.WaitGroup
 	wg.Add(1)
-	go pump(4, "gas", &wg)
+	go pump("gas", &wg, 1000, 5000, pumpCap)
 	wg.Add(1)
-	go pump(4, "diesel", &wg)
+	go pump("diesel", &wg, 1000, 5000, pumpCap)
 	wg.Add(1)
-	go pump(1, "lpg", &wg)
+	go pump("lpg", &wg, 1000, 5000, lpgCap)
 	wg.Add(1)
-	go pump(8, "electric", &wg)
+	go pump("electric", &wg, 3000, 10000, elCap)
+	wg.Wait()
 }
